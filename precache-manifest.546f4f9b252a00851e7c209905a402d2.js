f42ba696e5328ef9743fcf5e0fe1485261675a92
QuinlanKauffman <quinlank@udel.edu>
2020-10-03 19:50:30 -0400

    Updates

---
+++ precache-manifest.546f4f9b252a00851e7c209905a402d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7243138d2a5c0a7759ed2c472bfd319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cd8fcd72fceff722847",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "35f52c9c4f3f74427405",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8cd8fcd72fceff722847",
+    "url": "/static/js/2.7f916fec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7f916fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f52c9c4f3f74427405",
+    "url": "/static/js/main.31e6d4ac.chunk.js"
+  },
+  {
+    "revision": "7660510685a76db99451",
+    "url": "/static/js/runtime-main.833867a2.js"
+  }
+]);
